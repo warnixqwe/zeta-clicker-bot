eb81694c991ee28ee5bf80bfbb8455a80e292efc
warnixqwe <spagocheat@gmail.com>
2026-04-03 21:29:52 +0300

    Add admin panel

--- bot_main.py
+++ bot_main.py
@@ -1,4 +1,4 @@
- import asyncio
+import asyncio
 import logging
 from aiogram import Bot, Dispatcher
 from config import BOT_TOKEN
--- webapp_main.py
+++ webapp_main.py
@@ -1,4 +1,4 @@
- import os
+import os
 import uvicorn
 from web_app import app
 
