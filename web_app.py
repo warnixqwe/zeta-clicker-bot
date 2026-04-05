8868f36004e5005ae4eec9302db89f51a7bcbb43
warnixqwe <spagocheat@gmail.com>
2026-04-05 11:14:16 +0300

    Fix: исправлен Mini App, добавлены кейсы и бустеры

--- web_app.py
+++ web_app.py
@@ -7,6 +7,18 @@ import uvicorn
 
 app = FastAPI()
 
+from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(HTTPSRedirectMiddleware)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 class ClickData(BaseModel):
     user_id: int
     clicks: int
