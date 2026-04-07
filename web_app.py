1f7b57ca3667104600a6b8abf826d7a2f40e0a7a
warnixqwe <spagocheat@gmail.com>
2026-04-07 08:28:27 +0300

    Update web_app.py

--- web_app.py
+++ web_app.py
@@ -887,7 +887,7 @@ async def mini_app(user_id: int = 1):
     
     
     
-    if __name__ == "__main__":
+if __name__ == "__main__":
     import uvicorn
     port = int(os.environ.get("PORT", 8000))
     uvicorn.run(app, host="0.0.0.0", port=port)
